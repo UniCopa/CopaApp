dd2d8b8af2450023dcc87c847708974086f1cb31
Christiane Kuhn <christianekuhn92@web.de>
2013-06-10 20:48:07 +0200

    Updated MainActivity(RefreshButton)
    and corrected search.xml

--- src/main/java/unicopa/copa/app/gui/MainActivity.java
+++ src/main/java/unicopa/copa/app/gui/MainActivity.java
@@ -30,6 +30,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import unicopa.copa.base.event.SingleEvent;
 
 /**
@@ -38,6 +39,9 @@ import unicopa.copa.base.event.SingleEvent;
  * @author Christiane Kuhn
  */
 public class MainActivity extends Activity {
+
+    ArrayList<SingleEvent> sEvents = new ArrayList<SingleEvent>();
+    MainAdapter sEventAdapter;
 
     /**
      * creates Activity with a list of SingleEvents. By clicking on a
@@ -53,52 +57,36 @@ public class MainActivity extends Activity {
 
 	singleEventListView.setAdapter(null);
 
-	ArrayList<SingleEvent> sEvents = new ArrayList<SingleEvent>();
 	sEvents.add(new SingleEvent(1, 3, "HU 102", Calendar.getInstance()
 		.getTime(), "David", 4));
-	sEvents.add(new SingleEvent(2, 2, "HU 103", Calendar.getInstance()
+	sEvents.add(new SingleEvent(3, 2, "HU 103", Calendar.getInstance()
 		.getTime(), "Robin", 00));
-	sEvents.add(new SingleEvent(3, 4, "HU 104", Calendar.getInstance()
-		.getTime(), "Philip", 0));
-	sEvents.add(new SingleEvent(4, 3, "HU 105", Calendar.getInstance()
-		.getTime(), "Felix", 7));
-	sEvents.add(new SingleEvent(5, 4, "HU 106", Calendar.getInstance()
-		.getTime(), "Philipp", 66));
-	sEvents.add(new SingleEvent(6, 5, "HU 102", Calendar.getInstance()
-		.getTime(), "David", 80));
-	sEvents.add(new SingleEvent(7, 6, "HU 103", Calendar.getInstance()
-		.getTime(), "Tuki", 55));
-	sEvents.add(new SingleEvent(8, 7, "HU 104", Calendar.getInstance()
-		.getTime(), "Christiane", 45));
-	sEvents.add(new SingleEvent(9, 8, "HU 105", Calendar.getInstance()
-		.getTime(), "Jule", 4));
-	sEvents.add(new SingleEvent(10, 9, "HU 106", Calendar.getInstance()
-		.getTime(), "Max", 45));
-	sEvents.add(new SingleEvent(11, 1, "HU 102", Calendar.getInstance()
-		.getTime(), "Moritz", 33));
-	sEvents.add(new SingleEvent(12, 2, "HU 103", Calendar.getInstance()
-		.getTime(), "Anne", 123));
-	sEvents.add(new SingleEvent(13, 3, "HU 104", Calendar.getInstance()
-		.getTime(), "Marie", 34));
-	sEvents.add(new SingleEvent(14, 4, "HU 105", Calendar.getInstance()
-		.getTime(), "Silke", 76));
-	sEvents.add(new SingleEvent(15, 5, "HU 106", Calendar.getInstance()
-		.getTime(), "Jan", 90));
-	MainAdapter sEventAdapter = new MainAdapter(this, sEvents);
+
+	sEventAdapter = new MainAdapter(this, sEvents);
 	singleEventListView.setAdapter((ListAdapter) sEventAdapter);
 
 	singleEventListView.setOnItemClickListener(new OnItemClickListener() {
 	    @Override
-	    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-		    long arg3) {
+	    public void onItemClick(AdapterView<?> arg0, View arg1,
+		    int position, long arg3) {
+
 		Intent intent = new Intent(MainActivity.this,
 			SingleEventActivity.class);
-		intent.putExtra("selected", singleEventListView.getAdapter()
-			.getItem(arg2).toString());
+		SingleEvent clicked = (SingleEvent) singleEventListView
+			.getAdapter().getItem(position);
+		intent.putExtra("selectedID", clicked.getSingleEventID());
+
 		startActivity(intent);
 	    }
 	});
 
+    }
+
+    public void onRefreshButtonClick(View view) {
+	SingleEvent sEventNew = new SingleEvent(4, 3, "new Room", Calendar
+		.getInstance().getTime(), "new Supervisor", 4);
+	sEvents.add(sEventNew);
+	sEventAdapter.notifyDataSetChanged();
     }
 
     @Override
