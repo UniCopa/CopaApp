dc5a7f73f95a4948f2d3984bb41a93e89eef0b91
Martin.Rabe <loki1989@gmx.net>
2013-06-15 22:58:26 +0200

    Added exception handling.

--- src/main/java/unicopa/copa/app/gui/SearchResultEventActivity.java
+++ src/main/java/unicopa/copa/app/gui/SearchResultEventActivity.java
@@ -40,7 +40,7 @@ import android.widget.TextView;
 /**
  * On this activity the Events as results of a search are shown.
  * 
- * @author Christiane Kuhn
+ * @author Christiane Kuhn, Martin Rabe
  */
 public class SearchResultEventActivity extends Activity {
 
@@ -74,23 +74,29 @@ public class SearchResultEventActivity extends Activity {
 	    eventList = (ArrayList<Event>) scon.getEvents(eventGroupID,
 		    categoryId);
 	} catch (ClientProtocolException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    PopUp.exceptionAlert(this, getString(R.string.cp_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
 	} catch (APIException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    PopUp.exceptionAlert(this, getString(R.string.api_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
 	} catch (PermissionException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    PopUp.exceptionAlert(this, getString(R.string.per_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
 	} catch (RequestNotPracticableException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    PopUp.exceptionAlert(this, getString(R.string.rnp_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
 	} catch (InternalErrorException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    PopUp.exceptionAlert(this, getString(R.string.ie_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
+	} catch (IOException e) {
+	    PopUp.exceptionAlert(this, getString(R.string.io_ex),
+		    e.getMessage());
+	    // e.printStackTrace();
 	}
 
 	// end getEvents
